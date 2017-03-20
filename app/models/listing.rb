68b01565c012b8738399957715190f5a09f34137
ChongErWhey <erwhey@gmail.com>
2017-03-21 00:36:40 +0800

    Listings can be created & improved signup signin ajax

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,8 @@ class Listing < ApplicationRecord
   belongs_to :user
   has_many :tags, :through => :taggings
   has_many :taggings
+
+  validates :title, presence: true
+  validates :address, presence: true
+  validates :pax, presence: true
 end
